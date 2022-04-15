a4efdccf8f800d1fc33cbe5556670e42a6265756
Akiles <vareikismihail@gmail.com>
2022-04-15 18:38:28 +0300

    working

--- pkg/config/app.go
+++ pkg/config/app.go
@@ -10,7 +10,7 @@ var (
 )
 
 func Connect() {
-	d, err := gorm.Open("mysql", "dbuser:Qwerty123/simplerest?charset=utf8&parseTime=True&location=Local")
+	d, err := gorm.Open("mysql", "root:my_password@(192.168.77.101)/mysql?charset=utf8&parseTime=True&loc=Local")
 	if err != nil {
 		panic(err)
 	}
